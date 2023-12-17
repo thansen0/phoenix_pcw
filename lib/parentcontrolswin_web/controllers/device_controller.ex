2c8a7ed10281cc12ca9ed37b42242e0c56556c90
_ <thomash1996@live.com>
2023-12-16 21:46:31 -0600

    added user req for create and updated devices css

--- lib/parentcontrolswin_web/controllers/device_controller.ex
+++ lib/parentcontrolswin_web/controllers/device_controller.ex
@@ -1,21 +1,28 @@
 defmodule ParentcontrolswinWeb.DeviceController do
   use ParentcontrolswinWeb, :controller
+  import Ecto.Query, only: [from: 2]
 
   alias Parentcontrolswin.Devices
   alias Parentcontrolswin.Devices.Device
 
   def index(conn, _params) do
-    devices = Devices.list_devices()
+    user = Pow.Plug.current_user(conn)
+    # devices = Devices.list_devices()
+    devices = Parentcontrolswin.Repo.all(from d in Device, where: d.user_id == ^user.id)
     render(conn, :index, devices: devices)
   end
 
   def new(conn, _params) do
-    changeset = Devices.change_device(%Device{})
+    user = Pow.Plug.current_user(conn)
+    changeset = Devices.change_device(%Device{user_id: user.id})
     render(conn, :new, changeset: changeset)
   end
 
   def create(conn, %{"device" => device_params}) do
-    case Devices.create_device(device_params) do
+    user = Pow.Plug.current_user(conn)
+    modified_device_params = Map.put(device_params, "user_id", user.id)
+
+    case Devices.create_device(modified_device_params) do
       {:ok, device} ->
         conn
         |> put_flash(:info, "Device created successfully.")
--- lib/parentcontrolswin_web/controllers/page_controller.ex
+++ lib/parentcontrolswin_web/controllers/page_controller.ex
@@ -4,12 +4,10 @@ defmodule ParentcontrolswinWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home)
+    render(conn, :home) #, layout: false)
   end
 
   def contact(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
     render(conn, :contact)
   end
 end
