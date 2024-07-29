3a4a8bea8e7a62c11345c7a31472aec5495b2bd4
_ <thomash1996@live.com>
2024-07-28 22:31:25 -0500

    clean up

--- lib/parentcontrolswin_web/controllers/registration_controller.ex
+++ lib/parentcontrolswin_web/controllers/registration_controller.ex
@@ -1,18 +1,12 @@
 defmodule ParentcontrolswinWeb.RegistrationController do
   use ParentcontrolswinWeb, :controller
-#  use Pow.Phoenix.Controller
   alias Recaptcha
 
-#  plug :verify_recaptcha when action in [:create]
-
   def create(conn, %{"user" => user_params}) do
-    IO.inspect(conn.params["g-recaptcha-response"], label: "reCAPTCHA Response")
+    # IO.inspect(conn.params["g-recaptcha-response"], label: "reCAPTCHA Response")
 
     case Recaptcha.verify(conn.params["g-recaptcha-response"]) do
-    # case verify_recaptcha(conn.params["g-recaptcha-response"]) do
       {:ok, response} ->
-        IO.inspect(response, label: "IN OK RESP")
-        # super(conn, %{"user" => user_params}) # Call the default Pow registration logic
         conn
         |> Pow.Plug.create_user(user_params)
         |> case do
@@ -25,24 +19,11 @@ defmodule ParentcontrolswinWeb.RegistrationController do
             conn
             |> put_flash(:error, "CAPTCHA verification failed.")
             |> redirect(to: ~p"/registration/new", changeset: changeset)
-            #render(conn, "new.html", changeset: changeset)
         end
       {:error, message} ->
-        IO.inspect(message, label: "IN ERROR RESP")
         conn
         |> put_flash(:error, "CAPTCHA verification failed.")
         |> redirect(to: ~p"/registration/new")
-    end
-  end
-
-  def verify_recaptcha(recaptcha_response) do
-    case Recaptcha.verify(recaptcha_response) do
-      "" ->
-        IO.inspect("returned empty string") 
-        {:error, "Invalid captcha"}
-      _ ->
-        IO.inspect("returned full string")
-        {:ok}
     end
   end
 end
